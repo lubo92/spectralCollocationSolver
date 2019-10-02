3f296ae56eb2adb35a3afdb1a4587396735253f0
Wolfgang Lubowski <>
2019-10-02 17:22:20 +0200

    add functionality for better initial value search
    next up:
    - check if docu has to be updated

--- spectralCollocationSolver/spectralCollocationSolver.py
+++ spectralCollocationSolver/spectralCollocationSolver.py
@@ -1,5 +1,9 @@
 from barycentricLagrangeInterpolation import InterpolatingFunction
 from scipy.optimize import least_squares
+from scipy.optimize import differential_evolution
+from scipy.optimize import brute
+from scipy.optimize import basinhopping
+from scipy.optimize import shgo
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -46,5 +50,46 @@ class SpectralCollocationSolver(object):
         Returns:
         scipy.optimize.optimize.OptimizeResult: result of solution search
         '''
-        solution = least_squares(self.residuals,init,args=[args])
+        solution = least_squares(self.residuals,init,kwargs={"args":[args]},method='trf')
+        #solution = brute(self.residuals,init,args=[args])
         return solution
+    
+    
+    def createInit(self, init, args):
+        '''
+        searches for a globally optimal solution for a small number of sampling points,
+        in order to use this solution as inital values for a least squares optimization on a finer grid
+        
+        Parameters:
+        init (ndarray(nNodes)): initial values of interpolating function at grid points
+        args (tuple): tuple of args as neccessary in the "residuals" method
+        
+        Returns:
+        tbd
+        '''
+        #solution = least_squares(self.residuals,init,args=[args],method='trf')
+        #solution = brute(self.residuals,init,args=tuple([args]),Ns=10)
+        solution = basinhopping(self.residuals,init,minimizer_kwargs={"args":[args]})
+        #solution = differential_evolution(self.residuals,init,args=tuple([args]))
+        #solution = shgo(self.residuals,init,args=tuple([args]))
+        return solution
+    
+    def getInitValues(self, points, values, weights, nodes, noiseScale):
+        '''
+        evalutes the raw inital solution at the new grid points
+        
+        Parameters:
+        points(ndarray): grid points to evaluate function at
+        values(ndarray): values at grid points of raw solution
+        weights(ndarray): weights of raw solution
+        nodes(ndarray): nodes of raw solution
+        noiseScale(double): scaling factor of noise for perturbation of raw solution
+        
+        Returns:
+        ndarray: values of raw solution at points of new grid
+        '''
+        initValues = np.ones(len(points))
+        for i in range(len(points)):
+            initValues[i] = self.f.evaluateFunction(points[i], values, weights, nodes)
+        noiseCoeff = np.random.standard_normal(len(initValues))*noiseScale + 1
+        return initValues*noiseCoeff
